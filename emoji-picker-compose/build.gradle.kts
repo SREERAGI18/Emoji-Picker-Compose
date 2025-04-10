0533236a4eb66f19552dae76b316c1254e21e072
Sreeraj <sreerajvijayan2@gmail.com>
2025-04-10 21:07:22 +0530

    - remove jitpack and pom - release 0.0.4

--- emoji-picker-compose/build.gradle.kts
+++ emoji-picker-compose/build.gradle.kts
@@ -40,9 +40,9 @@ android {
 publishing {
     publications {
         create<MavenPublication>("release") {
-            groupId = "com.github.SREERAGI18"
+            groupId = "com.github.sreeragi"
             artifactId = "emoji-picker-compose"
-            version = "0.0.3"
+            version = "0.0.4"
 
             // Delay 'from components.release' until after evaluation
             afterEvaluate {
